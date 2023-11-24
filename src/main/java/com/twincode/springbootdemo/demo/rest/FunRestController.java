073d3084bae3aa9b87de33b3c2e9e00c024bde86
Bravee06 <bravee300@gmail.com>
2023-11-25 02:00:31 +0700

    spring-boot-demo

---
+++ src/main/java/com/twincode/springbootdemo/demo/rest/FunRestController.java
@@ -0,0 +1,23 @@
+package com.twincode.springbootdemo.demo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello(){
+        return coachName;
+    }
+
+
+
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+}
